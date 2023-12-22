f0375492ce66a43a81d35b9696ac042ecb898b7c
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-12-22 14:10:56 +0100

    ajout authent

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 @Module({
-  imports: [UsersModule, ProductsModule, OrdersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ProductsModule, OrdersModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    // Ajoutez votre logique de validation d'utilisateur ici
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
